be867b7cfc47b5cae2aca2c8b94a2b1701b7a51e
Davide Baldi <d.baldi88@gmail.com>
2024-02-26 13:53:56 +0100

    Routing...... WORKSSSSSgit add .!

--- index.js
+++ index.js
@@ -8,14 +8,18 @@ const app= express();
 const port= 3000;
 
 app.get("/", (req,res)=> {
-    res.write("home page connected") //like this just a s aplace holder
-})
+    res.render(
+        "pages/home.ejs",
+        {
+            pageTitle: "Welcome to D^3+B Zoo!"
+        })
+    });
 
 
 app.listen(port, console.log("SERVER STARTED"))
 
 app.use(express.static("public"));
-app.use("view engine", "ejs");
+app.set("view engine", "ejs");
 app.use("/birds", birdsRouter);
 app.use("/mammals", mammalsRouter);
 app.use("/reptiles", reptilesRouter)
