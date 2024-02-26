acf65bc6eb85eea1ea42ad82d201cdfd3a1206fb
Davide Baldi <d.baldi88@gmail.com>
2024-02-26 13:20:06 +0100

    basic routing created

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-import x from "express";
+import express from "express";
 
 const app= express();
 
 const port= 3000;
 
+app.get("/", (req,res)=> {
+    res.write("home page connected") //like this just a s aplace holder
+})
+
 
 app.listen(port, console.log("SERVER STARTED"))
+app.use(express.static("public"));
+//app.use("view engine", "ejs"); för ejs
